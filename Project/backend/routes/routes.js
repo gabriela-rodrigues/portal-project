53c3c06ba986ee395975b64d8b164af38757979f
gabriela-rodrigues <gabrielarodrigues502@hotmail.com>
2022-10-11 17:48:34 -0300

    conclusão do back-end

--- Project/backend/routes/routes.js
+++ Project/backend/routes/routes.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs");
 var data = require('../data/data.json');
+   
 
 router.get('/list', async (req, res) => {
     res.send(data);
 });
+
+router.post('/add', async (req, res) => {
+    const result = req.body
+    data.push(result)
+    fs.writeFile("./data/data.json", JSON.stringify(data), err => {
+         if (err) throw err; 
+        console.log("Done writing")
+    })
+    res.send("ok")
+})
 
 module.exports = router;
